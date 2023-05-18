9f4d00e521819bfe7d9410bf3b7e70ebc5ecfa86
belgio99 <17623601+belgio99@users.noreply.github.com>
2023-05-18 15:51:33 +0200

    avanti

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def parseArgs():
     parser.add_argument("--istio-file", help="If you already have a custom Istio file, you can specify it here. You cannot use this option and --istio-profile at the same time.")
     parser.add_argument("--k8s-namespace",  default="default", help="The Kubernetes namespace to use. If not specified, the default namespace will be used.")
     parser.add_argument("--auto-sc-inject", action="store_true", help="Enable automatic sidecar injection for the specified namespace. If not specified, automatic sidecar injection will be disabled for the specified namespace.")
-    #parser.add_argument("--dry-run", action="store_true", help="Dry run the YAML file without applying it")
     return parser.parse_args()
 
 
