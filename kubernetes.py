7587373dab5559c6993a0a022f9565cc1959f9d2
belgio99 <17623601+belgio99@users.noreply.github.com>
2023-06-08 14:02:38 +0200

    post-clean

--- kubernetes.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-#def enableSidecarInjection(namespace):
-#    print("Enabling automatic sidecar injection for namespace " + namespace)
-#    subprocess.run(["kubectl label namespace", namespace, "istio-injection=enabled"], check=True)
-
